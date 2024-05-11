c40a6962e0b8601f191e87f1a9304f8702ddd83d
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-05-11 22:13:15 +0500

    no commits

---
+++ src/Skeleton.jsx
@@ -0,0 +1,34 @@
+// import { DownOutlined } from "@ant-design/icons";
+// import { Dropdown, Space } from "antd";
+// const items = [
+//   {
+//     label: <a href="https://www.antgroup.com">Delete</a>,
+//     key: "0",
+//   },
+//   {
+//     label: <a href="https://www.aliyun.com">Edit</a>,
+//     key: "1",
+//   },
+//   {
+//     type: "divider",
+//   },
+//   {
+//     label: "cansel",
+//     key: "3",
+//   },
+// ];
+// const Dawn = () => (
+//   <Dropdown
+//     menu={{
+//       items,
+//     }}
+//     trigger={["click"]}
+//   >
+//     <a onClick={(e) => e.preventDefault()}>
+//       <Space>
+//         <DownOutlined />
+//       </Space>
+//     </a>
+//   </Dropdown>
+// );
+// export default Dawn;
